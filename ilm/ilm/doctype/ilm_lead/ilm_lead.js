fc3223861cad7e371f4d48452cd2a8b86427d9f2
Azr-S <ashar.s@tasshamjit.com>
2025-05-01 03:24:17 +0400

    Initial Setup

---
+++ ilm/ilm/doctype/ilm_lead/ilm_lead.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ashar Shamsudheen and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ILM Lead", {
+// 	refresh(frm) {
+
+// 	},
+// });
